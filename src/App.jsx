00e82cd06be2e1b9d9677bdb8114d2f385c18580
Johanne Khan Malek <johannekhanmalek@johannes-mbp.lan>
2023-11-02 18:00:44 +0100

    with components

--- src/App.jsx
+++ src/App.jsx
@@ -2,124 +2,19 @@ import { useState } from "react";
 
 import "./App.css";
 
+import Form from "./components/Form";
+import StepTwo from "./components/StepTwo";
+
 function App() {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmedPassword, setConfirmedPassword] = useState("");
   const [formIsFilled, setFormIsFilled] = useState(false);
-  const [displayStyleForm, setDisplayStyleForm] = useState("to-display");
-  const [displayStyleResults, setDisplayStyleResults] = useState("to-hide");
-  const [infoToEdit, setInfoToEdit] = useState(false);
-
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
-  };
-
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const handlePasswordChange = (event) => {
-    console.log(event.target.value);
-    setPassword(event.target.value);
-  };
-
-  const handleConfirmedPasswordChange = (event) => {
-    setConfirmedPassword(event.target.value);
-  };
 
   // state qui gère l'affichage ou non d'un message d'erreur si les deux mots de passe ne sont pas les mêmes
 
   const [showError, setShowError] = useState(false);
 
-  // vérifier que le 2ème mdp est le même que le premier
-
-  const handleSubmit = (event) => {
-    console.log(event);
-    event.preventDefault();
-
-    if (confirmedPassword !== password) {
-      setShowError(true);
-      alert("Vos deux mots de passe ne sont pas identiques");
-    } else {
-      setFormIsFilled(true);
-      setDisplayStyleForm("to-hide");
-      setDisplayStyleResults("to-display");
-    }
-
-    console.log(infoToEdit);
-  };
-
-  const handleEdit = (event) => {
-    event.preventDefault();
-    setFormIsFilled(false);
-  };
-
   return (
     <>
-      <header> {formIsFilled ? "Results" : "Create Account"} </header>
-
-      {formIsFilled ? (
-        <div className="container">
-          <div>
-            <form onSubmit={handleEdit}>
-              <p> Name : {username}</p>
-              <p> Email : {email}</p>
-              <p> Password : {password}</p>
-              <input
-                className="edit-button"
-                type="submit"
-                value="Edit your information"
-              />
-            </form>
-          </div>
-        </div>
-      ) : (
-        <div className="container">
-          <div>
-            <form onSubmit={handleSubmit}>
-              <span className="desc">Name</span>
-
-              <input
-                type="text"
-                placeholder="Jean Dupont"
-                name="username"
-                onChange={handleUsernameChange}
-                value={username}
-              />
-              <span className="desc">Email</span>
-
-              <input
-                type="email"
-                placeholder="jean.dupont@lereacteur.io"
-                name="email"
-                onChange={handleEmailChange}
-                value={email}
-              />
-              <span className="desc">Password</span>
-
-              <input
-                type="password"
-                placeholder="azertyui"
-                name="password"
-                onChange={handlePasswordChange}
-                value={password}
-              />
-              <span className="desc">Confirm password</span>
-
-              <input
-                type="password"
-                placeholder="azertyui"
-                name="confirmed-password"
-                onChange={handleConfirmedPasswordChange}
-                value={confirmedPassword}
-              />
-              <input className="submit-button" type="submit" value="Register" />
-            </form>
-          </div>
-        </div>
-      )}
+      {formIsFilled ? <StepTwo /> : <Form />}
 
       <footer>
         Made with <span>React</span> at <span>Le Reacteur</span> by
